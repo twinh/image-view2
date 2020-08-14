539df4155bf4f6db1457b8cc6ef73ccbc258566d
twinh <twinhuang@qq.com>
2020-08-14 18:00:40 +0800

    test: jest 配置移到 package.json 中

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  transform: {
-    '^.+\\.jsx?$': 'babel-jest',
-  },
-  testEnvironment: 'jsdom',
-  testTimeout: 10000,
-  // Allow /node_modules/ for CI testing
-  'transformIgnorePatterns': [],
-};
